33bcdcc881fee96f9cdcf0f0dddeebc93b1902ad
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-03-09 15:29:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
-$("#show-profile").click(function(){
+//see skills
+    $("#show-profile").click(function(){
 
-       // $("#span-profile").show();
+     
     $('#span-profile').slideToggle("slow");
     
    
 });
+
+
+//div
+
+$("#show-info").click(function(){
+
+    $('.hidden-info').slideToggle("slow");
+    
+   });
+
+//$("#show-info").mouseover(function(){
+
+// $('.hidden-info').slideToggle("slow");
+ 
+//});
+//hidden div information
+$("#go-back-to-card").mouseover(function(){
+$('.hidden-info').slideUp("fast");
+});
+$("#go-back-to-card").click(function(){
+    $('.hidden-info').slideUp("fast");
+    });
+//go back from table
+$('#go-back-from-table').click(function(){
+$('#span-profile').slideUp('fast');
+});
+//see projects
+$('#show-projects').click(function(){
+$('#hidden-projects').show();
+});
+
 });
